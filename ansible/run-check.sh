630657ee8bedf8408bde43225beb47339746ffe7
Tempest371 <george.wang333@gmail.com>
2023-05-05 14:53:36 +1000

    Changes after restart

--- ansible/run-check.sh
+++ ansible/run-check.sh
@@ -2,4 +2,7 @@
 
 # ansible-galaxy collection install openstack.cloud:2.0.0
 
-. ./secrets.sh; ansible-playbook -u ubuntu -vvv dynamic_scaling.yaml --check
+. ./secrets.sh; ansible-playbook -u ubuntu docker_swarm.yaml
+
+# use -K for sudo password 
+
