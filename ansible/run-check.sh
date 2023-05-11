5559bae2b922261f4d8be1dd2d2c89bed630025b
Tempest371 <george.wang333@gmail.com>
2023-05-12 01:17:19 +1000

    mastodon change

--- ansible/run-check.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# ansible-galaxy collection install openstack.cloud:2.0.0
-
-. ./secrets.sh; ansible-playbook -u ubuntu docker_swarm.yaml
-
-# use -K for sudo password 
-
---
+++ ansible/run-mastodon.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+ansible-playbook -u ubuntu -i /home/wagw/COMP90024-Cluster-and-Cloud-Computing-Assignment-2/ansible/inventory/inventory.ini mastodon.yaml
+
+# use -K for sudo password 
+
