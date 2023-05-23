a9e7dd0170921a6c3c05245e85052a32cabb3260
george <george.wang333@gmail.com>
2023-05-23 17:03:14 +1000

    Only instance creation and volume attachment

--- ansible_docker/run-mastodon.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-. ./secrets.sh; ansible-playbook -u ubuntu -i ~/inventory/inventory.ini mastodon.yaml
-
--- ansible_docker/run-swarm.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-ansible-playbook -u ubuntu -i ~/inventory/inventory.ini docker_swarm.yaml
-
-
-
